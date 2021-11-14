ec1fdac8c5f30a5caa9c1609fcb2b0eb23f38f82
SreelakshmiSajeevan <54982096+SreelakshmiSajeevan@users.noreply.github.com>
2021-11-14 15:48:33 +0530

    Add files via upload

---
+++ src/main/java/javamavenhibernate/TablePerConcreteclassusingAnnotation/App.java
@@ -0,0 +1,13 @@
+package javamavenhibernate.TablePerConcreteclassusingAnnotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
